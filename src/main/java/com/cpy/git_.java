6c373b062112324a00d670a7be739206c7debe83
Cpy <1711141335@qq.com>
2022-04-27 22:43:03 +0800

    first commit

---
+++ src/main/java/com/cpy/git_.java
@@ -0,0 +1,11 @@
+package com.cpy;
+
+/**
+ * @Author:C
+ * @create 2022-04-27-22:41
+ */
+public class git_ {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
